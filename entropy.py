64b99f44881165e62641e5d2fe837d49d2305c28
Calum Grant <calum74@gmail.com>
2025-04-13 19:25:33 +0100

    WIP

--- entropy.py
+++ entropy.py
@@ -24,29 +24,19 @@ def inefficient_convert(range):
 
 # Algorithm 3
 def combine(n_value, n_range, m_value, m_range):
-    '''
-    For two uniform distributions U(a) and U(b), 
-    return a combined distribution U(ab).
-    '''
     return n_value * m_range + m_value, n_range * m_range
 
 # Algorithm 4
 def factorize(n_value, n_range, m_range):
-    '''
-    The inverse of combine(): For a distribution U(ab),
-    return two distributions U(a) and U(b)
-    '''
     assert n_range % m_range == 0
-    return n_value%m_range, m_range, n_value//m_range, n_range//m_range
+    return n_value%m_range, m_range, \
+        n_value//m_range, n_range//m_range
 
 # Algorithm 5
-def resize(a_value, a_range, b_range):
-     '''
-     For a distribution U(a), return a distribution
-     of size U(b) or U(a-b)
-     '''
-     assert b_range <= a_range
-     return (a_value, b_range) if a_value<b_range else (a_value-b_range, a_range-b_range)
+def resize(n_value, n_range, m_range):
+     assert m_range <= n_range
+     return (n_value, m_range) if n_value<m_range \
+        else (n_value-m_range, n_range-m_range)
 
 def random_bit():
     return random.randint(0,1)
@@ -55,32 +45,30 @@ def random_bit():
 input_count = 0
 output_count = 0
 
-def fetch(a_value, a_range):
-    '''
-    Fetch one bit of entopy
-    It's not using hardware entropy
-    '''
+def fetch(n_value, n_range):
     global input_count
     input_count = input_count+1
-    return combine(a_value, a_range, random_bit(), 2)
+    return combine(n_value, n_range, random_bit(), 2)
 
-def convert(a_value, a_range, b_range, min_range):
+total_loss = 0
+
+def extract(n_value, n_range, m_range, min_range):
     first = True  # Work around do-while loops in Python
-    while a_range<b_range or first:
+    if min_range < m_range:
+        min_range = m_range
+    p = (min_range - m_range + 1)/min_range
+    global total_loss
+    total_loss = total_loss + (-math.log2(p) - (1-p)*math.log2(1-p)/p)
+    while n_range<m_range or first:
         first = False
-        while a_range<min_range or a_range < b_range:
+        while n_range<min_range:
             # Get one more bit of entropy
-            a_value, a_range = fetch(a_value, a_range)
-        a_value, a_range = resize(a_value, a_range, a_range%b_range)
-    return factorize(a_value, a_range, b_range)
+            n_value, n_range = fetch(n_value, n_range)
+        n_value, n_range = resize(n_value, n_range, n_range%m_range)
+    return factorize(n_value, n_range, m_range)
 
 
 class U:
-    '''
-    An entropy store.
-
-    This is a convenience wrapper around the primitive functions.
-    '''
     def __init__(self,value=0,range=1):
         self.value = value
         self.range = range
@@ -107,7 +95,7 @@ class U:
         '''
         Gets a random number.
         '''
-        r_value, r_range, a_value, a_range = convert(self.value, self.range, range, min_range)
+        r_value, r_range, a_value, a_range = extract(self.value, self.range, range, min_range)
         self.value = a_value
         self.range = a_range
         global output_count
@@ -123,18 +111,25 @@ class U:
     def entropy(self):
         return math.log2(self.range)
 
-# def shuffle(store, deck=52):
-#    for i in range(1,deck+1):
-#        store.get(i, 1<<24)
-
 store = U()
-# shuffle(store, 52)
 cards = list(range(52))
-# fisher_yates(cards, random)
 fisher_yates(cards, store)
 
 input_count = input_count - store.entropy()
 print("Output count", output_count)
 print("Input count", input_count)
-print("Efficiency", output_count/input_count)
+print("Actual efficiency", output_count/input_count)
+print("Total estimated loss", total_loss)
+print("Total estimated input", output_count + total_loss)
+print("Estimated efficiency", output_count/(output_count + total_loss))
 
+exit(0)
+
+n = 2**32
+m = 6
+p = (n-m+1)/n
+print(p)
+loss = -math.log2(p) - (1-p)*math.log2(1-p)/p
+print("Loss", loss)
+e = math.log2(m) / (math.log2(m) + loss)
+print("e", e)
