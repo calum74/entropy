0114b9a069fe339ab767373ce421bdc2d49412ea
Calum Grant <calum74@gmail.com>
2025-06-10 08:50:39 +0100

    WIP

--- entropy.cpp
+++ entropy.cpp
@@ -2,23 +2,6 @@
 #include <vector>
 #include <random>
 
-template<int N, int V=1, typename Src>
-int fast_dice_roller(Src&& src, int c=0)
-{
-    c = 2*c + src();
-    if constexpr ((2*V)>=N)
-    {
-        if (c<N)
-            return c;
-        else 
-            return fast_dice_roller<N, 2*V-N>(src, c-N);
-    }
-    else
-    {
-        return fast_dice_roller<N,2*V>(src, c);
-    }
-}
-
 // Binary entropy stream
 
 template<typename Generator>
@@ -70,118 +53,126 @@ private:
     value_type m_value, m_shift;
 };
 
-struct entropy_store
-{
-    using value_type = std::uint32_t;
-    value_type U_s = 0;
-    value_type s = 1;
-};
-
-template<std::unsigned_integral T = std::uint32_t, T M = (1u<<((sizeof(T)*8)-1))>
-class efficient_entropy_converter
+class distribution
 {
 public:
-    using value_type = T;
+    using uint32_t = std::uint32_t;
+    distribution(std::initializer_list<uint32_t> weights) : distribution(std::vector(weights)) {}
+    distribution(std::vector<uint32_t> w) : weights(std::move(w))
+    {
+        for(int i=0; i<weights.size(); ++i)
+        {
+            offsets.push_back(outputs.size());
+            for(int j=0; j<weights[i]; ++j)
+            {
+                outputs.push_back(i);
+            }
+        }
+    }
 
-    template<std::invocable Fn>
-    value_type convert(Fn fetch, value_type m)
+    std::vector<std::uint32_t> weights, outputs, offsets;
+};
+
+// For exposition only
+struct entropy_store
+{
+    using uint32_t = std::uint32_t;
+    binary_entropy_stream<std::random_device> input;
+
+    uint32_t fetch()
+    {
+        return *input++;
+    }
+
+    const uint32_t N = 1<<31;
+    uint32_t U_s = 0;
+    uint32_t s = 1;
+
+    uint32_t generate_uniform(uint32_t n)
     {
         for(;;)
         {
-            // Preload entropy one bit at a time into s
-            while(s_range < M)
+            // Preload entropy one bit at a time into U_s
+            while(s < N)
             {
-                s_value <<= 1;
-                s_value |= fetch();
-                s_range <<= 1;
+                U_s <<= 1;
+                U_s |= fetch();
+                s <<= 1;
             }
             // Resample entropy s to a multiple of m
-            auto r = s_range / m;
-            auto c = s_range % m;
-            if(s_value >= c)  [[likely]]
+            uint32_t r = s / n;
+            uint32_t c = s % n;
+            if(s >= c)  [[likely]]
             {
                 // Resample successful
-                s_value -= c;
-                auto a = s_value / m;
-                auto b = s_value % m;
-                s_value = a;
-                s_range = r; 
+                s -= c;
+                auto a = U_s / n;
+                auto b = U_s % n;
+                U_s = a;
+                s = r; 
                 return b;
             }
             else
             {
                 // Resample unsuccessful
-                s_range = c;
+                s = c;
             }
         }
     }
 
-    template<std::input_iterator It>
-    value_type convert(It & binary_entropy, value_type m)
+    void combine(uint32_t U_n, uint32_t n)
     {
-        return convert([&] { return *binary_entropy++; }, m);
+        U_s = U_s * n + U_n;
+        s = s * n;
     }
 
-    efficient_entropy_converter() : s_value(0), s_range(1) {}
-
-    efficient_entropy_converter(efficient_entropy_converter && other) : s_value(other.s_value), s_range(other.s_range)
+    uint32_t generate_distribution(uint32_t n, const uint32_t * weights, const uint32_t * outputs, const uint32_t * offsets)
     {
-        other.s_value = 0;
-        other.s_range = 1;
+        uint32_t U_n = generate_uniform(n);
+        uint32_t i = outputs[U_n];
+        // combine(U_n - offsets[i], weights[i]);
+        return i;
     }
 
-    efficient_entropy_converter & operator=(efficient_entropy_converter &&other)
+
+    void combine_distribution(uint32_t i, uint32_t n, const uint32_t * weights, const uint32_t * offsets)
     {
-        s_value = other.s_value;
-        s_range = other.s_range;
-        other = {};
-        return *this;
+        combine(offsets[i] + generate_uniform(weights[i]), weights[i]);
     }
 
-private:
-    value_type s_value = 0, s_range = 1;
+    uint32_t generate_distribution(const distribution & distribution)
+    {
+        return generate_distribution(
+            distribution.outputs.size(), 
+            distribution.weights.data(),
+            distribution.outputs.data(),
+            distribution.offsets.data());
+    }
+
+    void combine_distribution(const distribution & distribution, uint32_t i)
+    {
+        combine_distribution(i,
+            distribution.outputs.size(), 
+            distribution.weights.data(),
+            distribution.offsets.data());
+    }
+
+    uint32_t convert_distribution(const distribution &dist_input, const distribution &dist_output, uint32_t i)
+    {
+        combine_distribution(dist_input, i);
+        return generate_distribution(dist_output);
+    }
 };
 
-class distribution
-{
-public:
-    distribution(std::initializer_list<int> weights) : weights(std::move(weights))
-    {
-        for(int i=0; i<weights.size(); ++i)
-        {
-            auto start = outputs.size();
-            for(int j=0; j<weights[i]; ++j)
-            {
-                outputs.push_back(i);
-                starts.push_back(start);
-            }
-        }
-    }
-
-    std::vector<int> weights, outputs, starts;
-};
-
-int generate_distribution(entropy_store &s, const distribution &d)
-{
-
-}
 
 int main()
 {
-    std::random_device rd;
-    std::uniform_int_distribution u {0,1};
-    auto entropy = [&] { return u(rd); };
-    std::cout << fast_dice_roller<47>(entropy) << std::endl;
-
-    binary_entropy_stream<std::random_device> b;
-    efficient_entropy_converter<std::uint32_t> ec;
-
-    for(int i=0; i<100; ++i)
-        std::cout << *b++;
+    distribution d = {1,9};
+    entropy_store s;
+    for(int i=0; i<100; i++)
+        std::cout << s.generate_uniform(6);
     std::cout << std::endl;
-    for(int i=0; i<100; ++i)
-        std::cout << ec.convert(b, 6);
+    for(int i=0; i<100; i++)
+        std::cout << s.generate_distribution(d);
     std::cout << std::endl;
-
-    distribution d = {1,2};
 }
