a6a44a92eb0ab4f18e9bd8f93053c30211fb365d
Calum Grant <calum74@gmail.com>
2025-07-25 20:31:35 +0100

    Benchmarks

--- tests/bench.cpp
+++ tests/bench.cpp
@@ -1,46 +1,58 @@
 #include "entropy_store.hpp"
-#include <string_view>
-#include <iostream>
+#include "testing.hpp"
+
+#include <chrono>
 #include <cstdlib>
+#include <iostream>
+#include <string_view>
 
 using namespace std::literals;
 
 int usage()
 {
-    std::cout << "Usage: bench static|runtime count\n";
+    std::cout << "Usage: bench static|$output $count\n";
     return 1;
 }
 
 int main(int argc, const char **argv)
 {
-    if(argc!=3)
+    if (argc != 3)
     {
         return usage();
     }
     bool static_type = argv[1] == "static"sv;
-    bool dynamic_type = argv[1] == "runtime"sv;
     int count = std::atoi(argv[2]);
 
-    if(static_type == dynamic_type)
-    {
-        return usage();
-    }
-
+#if 1
+    // Buffer the entropy
+    entropy_store::random_device_generator rd;
+    entropy_store::wrapped_source source{rd, 100000};
+    entropy_store::bit_generator fetch{source};
+#else
+    // Read from std::random_device
     entropy_store::random_bit_generator fetch;
+#endif
+
     auto es = entropy_store::entropy_store{fetch};
+    auto start_time = std::chrono::high_resolution_clock::now();
 
-    if(static_type)
+    if (argv[1] == "static"sv)
     {
-        entropy_store::const_uniform<1,6> d6;
-        for(int i=0; i<count; i++)
+        entropy_store::const_uniform<1, 6> d6;
+        for (int i = 0; i < count; i++)
             es(d6);
     }
-    else
+    else if (int d = atoi(argv[1]))
     {
-        entropy_store::uniform_distribution d6(1,6);
-        for(int i=0; i<count; i++)
+        entropy_store::uniform_distribution d6(1, d);
+        for (int i = 0; i < count; i++)
             es(d6);
     }
 
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Elapsed " << std::chrono::duration<double>(end_time - start_time) << "\n";
+
+    std::cout << "Per output: " << (std::chrono::duration<double>(end_time - start_time) / count) << "\n";
     return 0;
 }
--- tests/testing.hpp
+++ tests/testing.hpp
@@ -1,82 +1,136 @@
+#include <iostream>
 
-    // Checks that multiple samplings of a uniform distribution
-    // occur in the expected range. According to the central limit theorem
-    // However, they are not independent.
-    class debug_uniform
+namespace entropy_store
+{
+// Checks that multiple samplings of a uniform distribution
+// occur in the expected range. According to the central limit theorem
+// However, they are not independent.
+class debug_uniform
+{
+  public:
+    using value_type = std::uint64_t;
+    void record(value_type value, value_type size)
     {
-    public:
-        using value_type = std::uint64_t;
-        void record(value_type value, value_type size)
-        {
-            assert(value < size);
-            total_value += value;
-            total_size += size - 1;
-            total_count++;
-            sum_s_squared += size * size - 1;
-        }
+        assert(value < size);
+        total_value += value;
+        total_size += size - 1;
+        total_count++;
+        sum_s_squared += size * size - 1;
+    }
 
-        double sd() const
-        {
-            return std::sqrt(sum_s_squared / 12.0);
-        }
-
-        double mean() const
-        {
-            return total_size / 2.0;
-        }
-
-        double get_relative_bias() const
-        {
-            return (total_value - mean()) / sd();
-        }
-
-        double get_percentage_bias() const
-        {
-            return 100.0 * (total_value - mean()) / total_value;
-        }
-
-        debug_uniform(const char *place) : place(place) {}
-
-        ~debug_uniform()
-        {
-            std::cout << place << ": uniform distribution bias = " << get_percentage_bias() << "%\n";
-        }
-
-    private:
-        const char *place;
-        std::uint64_t total_value = 0, total_size = 0, total_count = 0;
-        double sum_s_squared = 0;
-    };
-
-        template<entropy_generator Source>
-    class stored_source
+    double sd() const
     {
-    public:
-        using value_type = typename Source::value_type;
-        using distribution_type = typename Source::distribution_type;
-        using size_type = std::size_t;
-        using source_type = typename Source::source_type;
+        return std::sqrt(sum_s_squared / 12.0);
+    }
 
-        stored_source(const Source &source, size_type count) : m_source(source)
-        {
-            m_values.reserve(count);
-            for(; count>0; --count)
-                m_values.push_back(m_source());
-        }
+    double mean() const
+    {
+        return total_size / 2.0;
+    }
 
-        auto operator()()
-        {
-            if(m_values.empty()) throw std::runtime_error("Out of values!");
-            auto result = m_values.back();
-            m_values.pop_back();
-            return result;
-        }
+    double get_relative_bias() const
+    {
+        return (total_value - mean()) / sd();
+    }
 
-        auto distribution() const { return m_source.distribution(); }
+    double get_percentage_bias() const
+    {
+        return 100.0 * (total_value - mean()) / total_value;
+    }
 
-        int fetch_bit() { return m_source.fetch_bit(); }
+    debug_uniform(const char *place) : place(place)
+    {
+    }
 
-        std::vector<value_type> m_values;
-    private:
-        Source m_source;
-    };
+    ~debug_uniform()
+    {
+        std::cout << place << ": uniform distribution bias = " << get_percentage_bias() << "%\n";
+    }
+
+  private:
+    const char *place;
+    std::uint64_t total_value = 0, total_size = 0, total_count = 0;
+    double sum_s_squared = 0;
+};
+
+template <entropy_generator Source> class stored_source
+{
+  public:
+    using value_type = typename Source::value_type;
+    using distribution_type = typename Source::distribution_type;
+    using size_type = std::size_t;
+    using source_type = typename Source::source_type;
+
+    stored_source(const Source &source, size_type count) : m_source(source)
+    {
+        m_values.reserve(count);
+        for (; count > 0; --count)
+            m_values.push_back(m_source());
+    }
+
+    auto operator()()
+    {
+        if (m_values.empty())
+            throw std::runtime_error("Out of values!");
+        auto result = m_values.back();
+        m_values.pop_back();
+        return result;
+    }
+
+    auto distribution() const
+    {
+        return m_source.distribution();
+    }
+
+    int fetch_bit()
+    {
+        return m_source.fetch_bit();
+    }
+
+    std::vector<value_type> m_values;
+
+  private:
+    Source m_source;
+};
+
+template <entropy_generator Source> class wrapped_source
+{
+  public:
+    using value_type = typename Source::value_type;
+    using distribution_type = typename Source::distribution_type;
+    using size_type = std::size_t;
+    using source_type = Source;
+
+    wrapped_source(const Source &source, size_type count) : m_source(source)
+    {
+        m_values.reserve(count);
+        for (; count > 0; --count)
+            m_values.push_back(m_source());
+    }
+
+    auto operator()()
+    {
+        ++m_index;
+        if (m_index >= m_values.size())
+            m_index = 0;
+        return m_values[m_index];
+    }
+
+    auto distribution() const
+    {
+        return m_source.distribution();
+    }
+
+    int fetch_bit()
+    {
+        return m_source.fetch_bit();
+    }
+
+    std::vector<value_type> m_values;
+
+  private:
+    Source m_source;
+    std::size_t m_index = 0;
+};
+
+} // namespace entropy_store
